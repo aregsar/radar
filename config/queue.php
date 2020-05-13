f1ffa70e9fdff3ebe39c03de5813f6fee718b5b3
Areg Sarkissian <aregsar@gmail.com>
2020-05-13 10:58:58 -0700

    update

--- config/queue.php
+++ config/queue.php
@@ -12,8 +12,7 @@ return [
     | syntax for every one. Here you may define a default connection.
     |
     */
-
-    'default' => 'job',
+    'default' => env('QUEUE_CONNECTION', 'job'),
 
     /*
     |--------------------------------------------------------------------------
