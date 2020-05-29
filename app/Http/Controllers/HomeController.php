6d099bafe323661dfb554a20a75c79c2f0ceab76
Areg Sarkissian <aregsar@gmail.com>
2020-05-29 12:19:49 -0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,11 +8,6 @@ use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        //$this->middleware('auth');
-    }
-
 
     public function index()
     {
