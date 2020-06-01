853862d599b42acefac6829f5d6b20046a876b3a
Areg Sarkissian <aregsar@gmail.com>
2020-06-01 13:46:41 -0700

    update

--- app/Http/Controllers/Demo/Email.php
+++ app/Http/Controllers/Demo/Email.php
@@ -5,10 +5,17 @@ namespace App\Http\Controllers\Demo;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Mail\Mailable;
+use Illuminate\Support\Facades\Mail;
+
+
+
 class Email extends Controller
 {
     public function __invoke(Request $request)
     {
-        return "email demo";
+        Mail::to("");
+
+        return "email demossssssss";
     }
 }
