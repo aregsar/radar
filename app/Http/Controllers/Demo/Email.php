f8f0f85b6a0f9e22cbd7012686e1df077014135c
Areg Sarkissian <aregsar@gmail.com>
2020-06-02 12:51:42 -0700

    update

--- app/Http/Controllers/Demo/Email.php
+++ app/Http/Controllers/Demo/Email.php
@@ -9,21 +9,41 @@ use App\Http\Controllers\Controller;
 use App\Mail\Demo\WelcomeEmail;
 use App\Mail\Demo\WelcomeEmailMd;
 
+use App\Notifications\Demo\Welcome;
+use App\User;
+
+use App\Notifications\InvoicePaid;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Support\Facades\Auth;
+
 class Email extends Controller
 {
     //mail dashboard localhost:8025
     public function __invoke(Request $request)
     {
+
         $email = new WelcomeEmail;
 
         $emailmd = new WelcomeEmailMd;
 
-        Mail::to("x@g.com")->send($email);
+        //Mail::to("x@g.com")->send($email);
 
         //send using the default queue connection
         //if sync queue connection behaves same as send.
-        Mail::to("x@g.com")->queue($email);
+        //Mail::to("x@g.com")->queue($email);
 
-        return $email;
+        $welcomeNotification = new Welcome;
+
+        //$user->notify($welcomeNotification);
+
+        $user = User::where('email',Auth::user()->email)->first();
+        $user->notify(new InvoicePaid);
+
+        //Notification::send($user, new InvoicePaid);
+        //Notification::route('mail', 'x@g.com')->notify(new InvoicePaid);
+
+
+        //dd(Auth::user());
+        return $user;
     }
 }
